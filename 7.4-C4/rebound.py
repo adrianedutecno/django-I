5865d144c19960451dfe140ba763f14c50c4a421
afranco <adrian.franco@edutecno.com>
2023-07-01 12:12:51 -0400

    C4: busqueda, uso de django.db.models import Q, filtrado en base de datos

---
+++ 7.4-C4/rebound.py
@@ -0,0 +1,13 @@
+## Modificar campo de busqueda en la barra de navegacion (navbar)
+
+## Procesar los datos para realizar el filtrado en la base de datos
+    # crear view buscar_libro()
+
+# Template para buscar_libro.html
+
+# Desplegar los datos obtenidos de la busqueda en una lista
+
+# En cada libro obtenido poder editarlo
+
+# Configurar las urls o path para acceder a al views buscar_libro
+    
