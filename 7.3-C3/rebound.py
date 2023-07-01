39735dc012ba73500b8ae2f32f4ed66ab18996d9
afranco <adrian.franco@edutecno.com>
2023-06-30 20:57:02 -0400

    C3: metodo delete

---
+++ 7.3-C3/rebound.py
@@ -0,0 +1,9 @@
+# Continuar con el proyecto
+
+# Agregar boton de eliminar para el template lista_libros.html
+
+# Agregar views o controlador para eliminar
+
+# Configurar rutas o urls del proyecto
+
+# Módificaciones
