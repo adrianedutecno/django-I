663bae3658d93b794d900ef5156c08f6a826cc6d
afranco <adrian.franco@edutecno.com>
2023-07-03 21:42:56 -0400

    S8: UserCreationForm usando formulario de registro default django

---
+++ 8-S8/rebound/rebound.py
@@ -0,0 +1,18 @@
+# Views o controlador para el registro
+
+# Template de registro
+    # pip install django-crispy-forms -> renderizar formularios
+    # agregar 'crispy-forms' a installed apps en settings.
+        # INSTALLED_APPS = [ 'crispy-forms']
+        # CRISPY_TEMPLATE_PACK = "bootstrap5"
+    # crear template registro.html 
+    # pip install crispy-bootstrap5
+    # agregar 'crispy-forms' a installed apps en settings.
+        # INSTALLED_APPS = ["crispy_bootstrap5"]
+        # CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+# Configurar path o ruta de registro
+
+# template login.html
+
+# Custom Form para el registro
